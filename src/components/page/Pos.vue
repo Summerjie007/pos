104232a5900e7b5ec52ee8153272f9ef5c7b868f
lvxiaoxiao <2922712734@qq.com>
2018-06-12 17:20:18 +0800

    pos系统环境搭建

---
+++ src/components/page/Pos.vue
@@ -0,0 +1,24 @@
+<template>
+<!-- //Vue 2.0版本需要一个跟组件 -->
+  <div class="pos">
+  <h1>{{msg}}</h1>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Pos',
+  data () {
+    return {
+      msg: 'hello  vue  Pos 系统'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
